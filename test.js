22dae28ad77d2fc292c0130bc7c63bb59441d83d
Ernesto Rodriguez Castillo <developer1575@gmail.com>
2024-12-21 19:06:57 -0500

    Test

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ let separatae = strins.split(" ")[1].split("/")
 let body = separatae[1];
 let phone = separatae[0];
 
+
 let bodycomlete = strins.split(" ");
 for(i = 2 ; i<bodycomlete.length;i++)
 {
